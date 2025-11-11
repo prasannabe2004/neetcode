03cda109cc2b03cf444ae9099364fe77b9317db6
prasannabe2004 <prasannabe2004@gmail.com>
2025-11-11 06:55:29 +0530

    Add linked list problems

---
+++ containDuplicate.cpp
@@ -0,0 +1,40 @@
+/*
+ * Given an integer array nums, return true if any value appears at least twice in the array.
+ *
+ * Expectation: Return true if there are duplicates, false otherwise.
+ *
+ * Example:
+ * Input: nums = [1,2,3,1]
+ * Output: true
+ */
+
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool hasDuplicate(vector<int> &nums)
+    {
+        unordered_set<int> seen;
+        for (int n : nums)
+        {
+            if (seen.count(n))
+            {
+                return true;
+            }
+            seen.insert(n);
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums = {1, 2, 3, 1};
+    cout << s.hasDuplicate(nums) << endl;
+    return 0;
+}
---
+++ mergeLL.cpp
@@ -0,0 +1,78 @@
+/*
+ * Given two sorted linked lists, merge them into one sorted linked list.
+ * 
+ * Expectation: Return the head of the merged sorted linked list.
+ * 
+ * Example: 
+ * Input: l1 = [1,2,4], l2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ */
+
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+            return l1;
+
+        ListNode *dummy = new ListNode();
+        ListNode *head = dummy;
+
+        while (l1 && l2)
+        {
+            if (l1->val < l2->val)
+            {
+                dummy->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                dummy->next = l2;
+                l2 = l2->next;
+            }
+            dummy = dummy->next;
+        }
+        if (l1 != NULL)
+            dummy->next = l1;
+        if (l2 != NULL)
+            dummy->next = l2;
+        return head->next;
+    }
+};
+
+int main()
+{
+    ListNode *l1 = new ListNode(1);
+    l1->next = new ListNode(2);
+    l1->next->next = new ListNode(4);
+
+    ListNode *l2 = new ListNode(1);
+    l2->next = new ListNode(3);
+    l2->next->next = new ListNode(4);
+
+    Solution s;
+    ListNode *head = s.mergeTwoLists(l1, l2);
+    while (head != NULL)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+    return 0;
+}
