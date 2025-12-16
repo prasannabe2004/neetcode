dc1224ecf365ce9659c648579c1b0137e35deaf2
prasannabe2004 <prasannabe2004@gmail.com>
2025-12-16 14:42:17 +0530

    Arrays and Hashing complete

---
+++ arrayHashing/encodeDecode.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+/*
+Function to encode and decide a vector of strings into a single string
+Example:
+Input: ["HelloPrasana", "World"]
+Output: "5#HelloPrasana5#World"
+Explanation:
+The output string is the encoded string where the integers in the string
+indicates the number of characters to be skipped to form the next string in the
+array. If no integers are to be skipped, then no '#' will be in the output
+string. Example 2: Input: ["We", "say", ":", "yes"] Output: "2#We3#say2#:3#yes"
+Explanation:
+The output string is the encoded string where the integers in the string
+*/
+using namespace std;
+
+class Solution {
+   public:
+    string encode(vector<string>& strs) {
+        string res = "";
+        for (auto s : strs) {
+            res = res + to_string(s.length()) + "#" + s;
+        }
+        return res;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res;
+        int i = 0;
+        int length = s.length();
+        while (i < length) {
+            int j = i;
+            while (s[j] != '#') {
+                j++;
+            }
+            int len = stoi(s.substr(i, j - i));
+            res.push_back(s.substr(j + 1, len));
+            i = j + 1 + len;
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    vector<string> strs = {"HelloPrasana", "World"};
+    string r = s.encode(strs);
+    vector<string> out = s.decode(r);
+    for (auto s : out) {
+        cout << s << endl;
+    }
+    return 0;
+}
---
+++ arrayHashing/longestConsecutiveSequence.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+/*
+Find the longest consecutive elements sequence.
+Example 1:
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+*/
+
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        int max = 0;
+        unordered_set<int> set;
+        for (int num : nums) {
+            set.insert(num);
+        }
+        for (int num : nums) {
+            if (!set.count(num - 1)) {
+                int y = num + 1;
+                while (set.count(y)) {
+                    y++;
+                }
+                max = std::max(max, y - num);
+            }
+        }
+        return max;
+    }
+    int longestConsecutiveSorting(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int max = 0;
+        int count = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                count++;
+            } else if (nums[i] == nums[i - 1]) {
+                continue;
+            } else {
+                max = std::max(max, count);
+                count = 1;
+            }
+        }
+        return std::max(max, count);
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    cout << s.longestConsecutive(nums) << endl;
+    cout << s.longestConsecutiveSorting(nums) << endl;
+    return 0;
+}
