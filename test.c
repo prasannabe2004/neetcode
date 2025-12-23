785236cc2026a291853ea606b80307ca91310c79
prasannabe2004 <prasannabe2004@gmail.com>
2025-12-23 08:40:59 +0530

    update test.c

--- test.c
+++ test.c
@@ -1,20 +1,8 @@
-#include <stdint.h>
 #include <stdio.h>
 
-typedef struct {
-    uint32_t a;
-    float f;
-} u;
+#define NUM 10
 
-/*
-3f       80         00          00
-01111111 10000000   00000000    00000000
-*/
 int main() {
-    u uni;
-    uni.a = 1;
-    uni.f = 2.5;
-    printf("%d\n", uni.a);
-    printf("%.1f\n", uni.f);
+    printf("Hello World %d\n", NUM);
     return 0;
 }
