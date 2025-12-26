10744604f2bbb41d3872707b29e0f922b4957b29
prasannabe2004 <prasannabe2004@gmail.com>
2025-12-26 16:59:57 +0530

    Adding rotate array

---
+++ arrayHashing/rotateArray.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    void reverseArray(vector<int>& a, int start, int end) {
+        while (start <= end) {
+            swap(a[start], a[end]);
+            start++;
+            end--;
+        }
+    }
+    vector<int> rotateArray(vector<int>& nums, int d) {
+        d = d % nums.size();
+        reverseArray(nums, 0, nums.size() - 1);
+        // reverse(nums.begin(), nums.end());
+        reverseArray(nums, 0, d - 1);
+        // reverse(nums.begin(), nums.begin() + d);
+        reverseArray(nums, d, nums.size() - 1);
+        // reverse(nums.begin() + d, nums.end());
+        return nums;
+    }
+};
+
+int main() {
+    vector<int> a{1, 2, 3, 4, 5, 6, 7};
+    Solution s;
+    s.rotateArray(a, 2);
+    for (auto n : a) {
+        cout << n << " ";
+    }
+    cout << endl;
+    return 0;
+}
