78b1597143d7fe1bc134426cc69d92eed9102158
prasannabe2004 <prasannabe2004@gmail.com>
2026-01-21 21:47:08 +0530

    revisir longest unique substring and merge binary tree

---
+++ mergeBinaryTreesOverlapping.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+using namespace std;
+/*
+Merge Two Binary Trees by adding overlapping nodes
+https://leetcode.com/problems/merge-two-binary-trees
+You are given two binary trees root1 and root2. Imagine that when you put one of
+the trees to cover the other, some nodes of the two trees are overlapped while
+the others are not. You need to merge the two trees into a new binary tree. The
+merging rule is that if two nodes overlap, then sum node values up as the new
+value of the merged node. Otherwise, the NOT null node will be used as the node
+of the new tree.
+
+Example 1:
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+Example 2:
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
+*/
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if (root1 == nullptr && root2 == nullptr) {
+            return nullptr;
+        }
+        if (root1 == nullptr) {
+            return root2;
+        }
+        if (root2 == nullptr) {
+            return root1;
+        }
+
+        TreeNode* root = new TreeNode(root1->val + root2->val);
+        root->left = mergeTrees(root1->left, root2->left);
+        root->right = mergeTrees(root1->right, root2->right);
+        return root;
+    }
+};
+
+int main() {
+    Solution s;
+    TreeNode* root1 = new TreeNode(1);
+    root1->left = new TreeNode(3);
+    root1->right = new TreeNode(2);
+    root1->left->left = new TreeNode(5);
+
+    TreeNode* root2 = new TreeNode(2);
+    root2->left = new TreeNode(1);
+    root2->right = new TreeNode(3);
+    root2->left->right = new TreeNode(4);
+    root2->right->right = new TreeNode(7);
+
+    TreeNode* mergedRoot = s.mergeTrees(root1, root2);
+    cout << "Merged Root Value: " << mergedRoot->val << endl;  // Should print 3
+    cout << "Left Child Value: " << mergedRoot->left->val
+         << endl;  // Should print 4
+    cout << "Right Child Value: " << mergedRoot->right->val
+         << endl;  // Should print 5
+
+    return 0;
+}
--- slidingWindow/lengthOfLongestSubstring.cpp
+++ slidingWindow/lengthOfLongestSubstring.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_set>
+#include <vector>
 
 using namespace std;
 
@@ -7,8 +8,11 @@ using namespace std;
 LeetCode Problem: Length of Longest Substring Without Repeating Characters
 https://leetcode.com/problems/length-of-longest-substring-without-repeating-characters
 Given a string s, find the length of the longest substring without repeating
-characters. Example: Input: s = "abcabcbb" Output: 3 Explanation: The answer is
-"abc", with the length of 3.
+characters.
+
+Example: Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
 */
 
 class Solution {
@@ -36,34 +40,60 @@ class Solution {
         return res;
     }
     /*
+    Optimized sliding window approach using index array
+    Time Complexity: O(N)
+    Space Complexity: O(M) where M is the size of charset
+    */
+    int lengthOfLongestSubstring(string s) {
+        // vector<int> index(256, -1);  // ASCII size
+        vector<int> index(4, -1);
+        int maxLength = 0;
+        int left = 0;
+        for (int right = 0; right < s.size(); right++) {
+            cout << "Processing character: " << s[right] << endl;
+            for (auto val : index) {
+                cout << val << " ";
+            }
+            cout << endl;
+            int i = s[right] - 'a';
+            if (index[i] != -1) {
+                left = max(index[i] + 1, left);
+            }
+            index[i] = right;
+            maxLength = max(maxLength, right - left + 1);
+            cout << "Left: " << left << ", Right: " << right
+                 << ", Current Max Length: " << maxLength << endl;
+        }
+        return maxLength;
+    }
+
+    /*
     Sliding window approach
     Time Complexity: O(N)
     Space Complexity: O(min(M,N)) where M is the size of charset and N is the
     size of string
     */
-    int lengthOfLongestSubstring(string s) {
-        unordered_set<char> charSet;
-        int l = 0;
-        int r = 0;
-        int res = 0;
 
-        for (int i = 0; i < s.size(); i++) {
-            while (charSet.find(s[r]) != charSet.end()) {
-                charSet.erase(s[l]);
-                l++;
+    int lengthOfLongestSubstring2(string s) {
+        unordered_set<char> charSet;
+        int left = 0;
+        int maxLength = 0;
+        for (int right = 0; right < s.size(); right++) {
+            while (charSet.find(s[right]) != charSet.end()) {
+                charSet.erase(s[left]);
+                left++;
             }
-            charSet.insert(s[r]);
-            res = max(res, r - l + 1);
-            r++;
+            charSet.insert(s[right]);
+            maxLength = max(maxLength, right - left + 1);
         }
-        return res;
+        return maxLength;
     }
 };
 
 int main() {
     Solution solution;
-    string s = "abcabcbb";
-    cout << "Length of longest substring without repeating characters: "
+    string s = "abcabcd";
+    cout << "Length of longest substring without repeating characters: " << endl
          << solution.lengthOfLongestSubstring(s) << endl;
     return 0;
 }
