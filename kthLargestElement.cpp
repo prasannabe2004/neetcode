e31941e41bbf07e0e44a32640c87c09c0cfee8d9
prasannabe2004 <prasannabe2004@gmail.com>
2026-01-24 17:46:47 +0530

    Complete first dp and kthlargest element

--- kthLargestElement.cpp
+++ kthLargestElement.cpp
@@ -1,1 +1,16 @@
-class
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+        for (auto n : nums) {
+            minHeap.push(n);
+        }
+        return minHeap.pop();
+    }
+};
