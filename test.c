da05b9d3223aaa1591becf76231187777a95686e
prasannabe2004 <prasannabe2004@gmail.com>
2026-01-31 08:46:13 +0530

    clean up and gemini auto complete tested.

--- test.c
+++ test.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+/*
+Function to find sum
+*/
+int sum(int arr[], int n) {
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        res += arr[i];
+    }
+    return res;
+}
 
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
