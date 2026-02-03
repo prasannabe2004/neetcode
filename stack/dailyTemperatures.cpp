5919f5243b1deb5754a2593e23233185b8576219
prasannabe2004 <prasannabe2004@gmail.com>
2026-02-03 11:24:09 +0530

    fix compilation issues

--- stack/dailyTemperatures.cpp
+++ stack/dailyTemperatures.cpp
@@ -52,7 +52,7 @@ class Solution {
 int main() {
     Solution s;
     vector<int> temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
-    vector<int> result = s.dailyTempatures(temperatures);
+    vector<int> result = s.dailyTemperatures(temperatures);
     for (int i = 0; i < result.size(); i++) {
         cout << result[i] << " ";
     }
